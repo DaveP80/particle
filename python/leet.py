c65d0edc9383efde1297963846cc7369615d176c
DaveP80 <davidpaq1@gmail.com>
2023-04-29 18:30:31 -0400

    fixed code in leet.py

--- python/leet.py
+++ python/leet.py
@@ -2,12 +2,16 @@
 # maintaining the relative order of the non-zero elements.
 # Note that you must do this in-place without making a copy of the array.
 def moveZeroes(nums):
-        storage = []
-        for n in nums:
-            if n==0:
-                storage.append(0)
-                nums.remove(0)
-        print(len(nums))
-        nums += storage
+        zero_count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] == 0:
+                zero_count += 1
+            else:
+                nums[i - zero_count] = nums[i]
+        
+        for i in range(n - zero_count, n):
+            nums[i] = 0
         print(nums)
 moveZeroes([0,0,1])
