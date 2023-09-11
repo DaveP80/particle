bf89004864428d22ed84581f210aad428985affa
DaveP80 <davidpaq1@gmail.com>
2023-09-10 23:59:32 -0400

    combination sum iv

---
+++ javascript/combinationsum.js
@@ -0,0 +1,15 @@
+// LeetCode No. 377
+var combinationSum4 = function(nums, target) {
+    const dp = new Array(target + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= target; i++) {
+        for (const num of nums) {
+            if (i >= num) {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+
+    return dp[target];
+    }
